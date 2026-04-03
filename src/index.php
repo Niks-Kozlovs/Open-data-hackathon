1e2fe0e7aa90587a531d762784cd07d883f8f9f3
Niks Kozlovs <kozlovs.niks1@gmail.com>
2026-04-03 15:16:51 +0300

    Update tileLayer URL to use HTTPS for improved security

--- src/index.php
+++ src/index.php
@@ -112,7 +112,7 @@ include "functions/functions.php";
                     zoom: 7
                 });
 
-                L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
                     subdomains: ['a', 'b', 'c']
                 }).addTo(map);
